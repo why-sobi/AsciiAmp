bca5320dd50c1334a039ee005cb3b9e65c95fa61
why-sobi <sobisoban05@gmail.com>
2025-12-24 18:05:15 +0500

    Initial Commit

---
+++ include/temp.hpp
@@ -0,0 +1,22 @@
+# pragma once
+
+#include <filesystem>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+std::vector<fs::path> getMP3Files(const std::string& dir) {
+    std::vector<fs::path> mp3Files;
+
+    if (!fs::exists(dir) || !fs::is_directory(dir)) {
+        throw std::invalid_argument("Directory does not exist: " + dir);
+    }
+
+    for (const auto& entry : fs::directory_iterator(dir)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".mp3") {
+            mp3Files.push_back(entry.path());
+        }
+    }
+
+    return mp3Files;
+}
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+// standard includes
+#include <iostream>
+#include <vector>
+#include <random>
+
+// 3rd party includes
+#include <termviz.hpp>
+#include <taglib/fileref.h>
+
+// project includes
+#include <temp.hpp>
+
+int main() {
+    const std::string dir = "C:/Users/shadows box/Downloads";
+    std::vector<fs::path> mp3Files = getMP3Files(dir);
+
+    std::cout << "Total MP3 files: " << mp3Files.size() << '\n';
+
+    for (const fs::path& mp3File : mp3Files) {
+        TagLib::FileRef f(mp3File.c_str());
+        if (!f.isNull() && f.tag()) {
+            TagLib::Tag* tag = f.tag();
+            std::cout << "Title: " << tag->title().to8Bit(true) << std::endl;
+            std::cout << "Artist: " << tag->artist().to8Bit(true) << std::endl;
+            std::cout << "Album: " << tag->album().to8Bit(true) << std::endl;
+            std::cout << "--------------------------" << std::endl;
+        }
+    }
+}
+
+
+/*
+Put all headers in the include dir
+libs\eigen
+*/
+
